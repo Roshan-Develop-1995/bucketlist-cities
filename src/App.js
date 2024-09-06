a95155821e14f79c9e01f3413d23795acbb22ab5
Roshan-develop-1995 <pathak.ros12345@gmail.com>
2024-09-06 10:10:32 +0530

    Search component addition and performance improvement

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   constructor() {
     super();
-
     this.state = {
       cities: [],
+      searchField: ''
     }
   }
 
   componentDidMount() {
     fetch('http://localhost:8080/v1/cities')
       .then((res) => res.json())
-      .then((list) => this.setState(() => {
-        return {cities: list}
-      },
-      () => {
-        console.log(this.state);
-      }
-    ));
+      .then((list) => this.setState(
+        () => {
+          return { cities: list }
+        },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
+  // Optimization - by not rendering extra anonymous function on rendering
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
   }
 
 
   render() {
+    //For readability
+    const { cities, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const fileteredCities = cities.filter(
+      (city) => {
+        return city.cityName.toLowerCase().includes(searchField);
+      });
+
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search city'
+          onChange={onSearchChange}
+        />
         {
-          this.state.cities.map((city) => {
-            return <div key={city.id}>
-              <h1>{city.cityName}</h1>
-            </div>
-          })
+          fileteredCities.map(
+            (city) => {
+              return <div key={city.id}>
+                <h1>{city.cityName}</h1>
+              </div>
+            })
         }
       </div>
     );
